56e1536f7b2434af35afe7545247c48eee230beb
Johann Le Biannic <jlebiannic@sodifrance.fr>
2023-06-15 22:39:43 +0200

    refactor: SearchForm.vue

---
+++ src/component/SearchForm.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { onMounted } from "vue";
+import { useDisplay } from "vuetify/lib/framework.mjs";
+
+export interface ISearchFormProps {
+  onSubmit: (searchQuery: string) => void;
+}
+const props = defineProps<ISearchFormProps>();
+
+const searchQuery = ref("");
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const { mobile } = useDisplay();
+let isMobile = mobile.value;
+onMounted(() => {
+  forceFocusOnInput();
+  isMobile = mobile.value;
+});
+
+function forceFocusOnInput() {
+  // Permet de résoudre le problème suivant:
+  // Lorsqu'on clique sur une vidéo alors on est redirigé vers le site de youtube.
+  // Si on fait "history back" alors on est redirigé vers l'appli mais le champs input de recherche reste valorisé avec
+  //   l'ancienne valeur de recherche alors que la variable vuejs "searchQuery" est réinitialisée à vide
+  //   (en déphasage donc avec le contenu du champs input).
+  // En différant le focus (settimeout), la valeur disparait car elle est remise à la valeur de la variable vuejs "searchQuery"
+  //setFocuOnInput();
+  //inputRef.value?.blur();
+  setTimeout(() => {
+    setFocuOnInput();
+  }, 10);
+}
+
+function setFocuOnInput() {
+  inputRef.value?.focus();
+}
+</script>
+
+<template>
+  <v-row class="search-bar">
+    <v-col cols="9">
+      <v-text-field
+        ref="inputRef"
+        v-model="searchQuery"
+        placeholder="Enter text for youtube search"
+        @keyup.enter="() => props.onSubmit(searchQuery)"
+        clearable
+        label="Search on youtube"
+        variant="outlined"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="3">
+      <v-btn color="primary" block class="mt-2" @click="props.onSubmit(searchQuery)">
+        <v-icon
+          class="search-icon mt-1"
+          icon="mdi-card-search-outline"
+          size="x-large"
+          start
+        ></v-icon>
+        {{ isMobile ? "" : "Search" }}
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.search-bar {
+  max-height: 7rem;
+}
+
+.search-icon {
+  margin-top: 1.3rem;
+}
+</style>
--- src/view/SearchView.vue
+++ src/view/SearchView.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
 import YoutubeSearchResult from "@/component/YoutubeSearchResult.vue";
 import { YoutubeService, type ISearchResult } from "@/common/service/YoutubeService";
-import { onMounted, reactive, ref } from "vue";
+import { reactive, ref } from "vue";
 import YoutubeSearchResultMin from "@/component/YoutubeSearchResultMin.vue";
-import { useDisplay } from "vuetify/lib/framework.mjs";
 import DownloadButton from "@/component/DownloadButton.vue";
+import SearchForm from "@/component/SearchForm.vue";
 
-const inputRef = ref<HTMLInputElement | null>(null);
-
-const searchQuery = ref("");
 const youtubeSearchResults: ISearchResult[] = reactive([]);
 const youtubeInProgressDownloads: ISearchResult[] = reactive([]);
 
-const { mobile } = useDisplay();
 const searching = ref(false);
-let isMobile = mobile.value;
 
-onMounted(() => {
-  forceFocusOnInput();
-  isMobile = mobile.value;
-});
-
-async function searchOnYoutube() {
+async function searchOnYoutube(searchQuery: string) {
   searching.value = true;
   try {
-    const results = await YoutubeService.searchOnYouTube(searchQuery.value);
+    const results = await YoutubeService.searchOnYouTube(searchQuery);
     youtubeSearchResults.splice(0);
     youtubeSearchResults.push(...results);
   } finally {
@@ -39,24 +29,6 @@ async function downloadAudioTrack(searchResult: ISearchResult) {
   } finally {
     removeSearchResultToInProgressDownloads(searchResult);
   }
-}
-
-function setFocuOnInput() {
-  inputRef.value?.focus();
-}
-
-function forceFocusOnInput() {
-  // Permet de résoudre le problème suivant:
-  // Lorsqu'on clique sur une vidéo alors on est redirigé vers le site de youtube.
-  // Si on fait "history back" alors on est redirigé vers l'appli mais le champs input de recherche reste valorisé avec
-  //   l'ancienne valeur de recherche alors que la variable vuejs "searchQuery" est réinitialisée à vide
-  //   (en déphasage donc avec le contenu du champs input).
-  // En différant le focus (settimeout), la valeur disparait car elle est remise à la valeur de la variable vuejs "searchQuery"
-  //setFocuOnInput();
-  //inputRef.value?.blur();
-  setTimeout(() => {
-    setFocuOnInput();
-  }, 10);
 }
 
 function addSearchResultToInProgressDownloads(searchResult: ISearchResult) {
@@ -88,31 +60,7 @@ function isVideoIdIsInProgressDownloads(videoId: string): boolean {
     <v-form @submit.prevent autocomplete="on">
       <v-row>
         <v-col cols="11">
-          <!-- Champs de recherche -->
-          <v-row class="search-bar">
-            <v-col cols="9">
-              <v-text-field
-                ref="inputRef"
-                v-model="searchQuery"
-                placeholder="Enter text for youtube search"
-                @keyup.enter="searchOnYoutube"
-                clearable
-                label="Search on youtube"
-                variant="outlined"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="3">
-              <v-btn color="primary" block class="mt-2" @click="searchOnYoutube">
-                <v-icon
-                  class="search-icon mt-1"
-                  icon="mdi-card-search-outline"
-                  size="x-large"
-                  start
-                ></v-icon>
-                {{ isMobile ? "" : "Search" }}
-              </v-btn>
-            </v-col>
-          </v-row>
+          <SearchForm :on-submit="(searchQuery:string)=>searchOnYoutube(searchQuery)"></SearchForm>
           <!-- Résultats de la recherche -->
           <v-row v-if="searching">
             <v-col cols="6" offset="4">
@@ -162,16 +110,7 @@ function isVideoIdIsInProgressDownloads(videoId: string): boolean {
   margin: 1rem;
   color: red;
 }
-
 .search-result {
   margin-bottom: 10px;
-}
-
-.search-icon {
-  margin-top: 1.3rem;
-}
-
-.search-bar {
-  max-height: 7rem;
 }
 </style>
