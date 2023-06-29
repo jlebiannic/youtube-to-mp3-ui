cb7d6116087989a5dc2703190a970d22224d9444
Johann Le Biannic <jlebiannic@sodifrance.fr>
2023-06-29 22:37:00 +0200

    feat: css, color, icon

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -18,7 +18,7 @@ export default createVuetify({
       light: {
         colors: {
           primary: "#1867C0",
-          secondary: "#5CBBF6"
+          secondary: "#84C3FF"
         }
       }
     }
--- src/view/MainView.vue
+++ src/view/MainView.vue
@@ -2,18 +2,18 @@
 import { ref } from "vue";
 import SearchView from "./SearchView.vue";
 import { shallowRef } from "vue";
+import { useTheme } from "vuetify/lib/framework.mjs";
 const tab = ref(0);
 const drawer = ref(false);
 const items = shallowRef([
   { idx: 0, title: "Search", icon: "mdi-magnify", content: SearchView },
-  { idx: 1, title: "Search history", icon: "mdi-account-box", content: SearchView }
+  { idx: 1, title: "Search history", icon: "mdi-history", content: SearchView }
 ]);
+const theme = useTheme();
+const secondaryColor = theme.global.current.value.colors.secondary;
 </script>
 
 <template>
-  <!-- <v-card>
-    <v-layout> -->
-  <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
   <v-layout>
     <v-app-bar color="primary" prominent>
       <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -23,7 +23,11 @@ const items = shallowRef([
       <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" temporary>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      :style="{ height: '10rem', backgroundColor: secondaryColor }"
+    >
       <v-list dense nav>
         <v-list-item
           v-for="item in items"
@@ -34,22 +38,28 @@ const items = shallowRef([
             tab = item.idx;
           "
         >
-          <v-icon>{{ item.icon }}</v-icon>
-
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-row no-gutters align="center">
+            <v-icon
+              :style="{
+                marginRight: '0.1rem'
+              }"
+              >{{ item.icon }}</v-icon
+            >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-row>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-main style="height: 100px">
-      <v-window v-model="tab">
-        <v-window-item v-for="item in items" :key="item.title">
-          <component :is="item.content"></component>
-        </v-window-item>
+      <v-window v-model="tab"
+        ><v-card-text>
+          <v-window-item v-for="item in items" :key="item.title">
+            <component :is="item.content"></component>
+          </v-window-item>
+        </v-card-text>
       </v-window>
     </v-main>
-    <!-- </v-layout>
-    </v-card> -->
   </v-layout>
 </template>
 
